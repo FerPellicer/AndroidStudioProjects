51319c9e17d50780f7c2de06459389b02cce6e23
Fernando <fernandopellicer782@gmail.com>
2022-10-18 20:32:12 +0100

    Actualizaciones Lección 1

--- Lección 1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Lección 1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,26 +16,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         diceImage = findViewById(R.id.dice_image)
+        diceImage = findViewById(R.id.dice_image2)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
     }
 
-
-    private fun rollDice() {
-
-        val randomInt = (1..6).random()
-        val diceImage : ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when (randomInt) {
+    private fun getRandomDiceImage(value: Int) : Int {
+       val drawableResource = when (value) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
-        }
-        diceImage.setImageResource(drawableResource)
+       }
+       return  drawableResource
+
+    }
+
+    private fun rollDice() {
+
+        var randomInt = (1..6).random()
+
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+
+        diceImage.setImageResource(getRandomDiceImage(randomInt))
+
+        randomInt = (1..6).random()
+
+        val diceImage1 : ImageView = findViewById(R.id.dice_image2)
+
+        diceImage1.setImageResource(getRandomDiceImage(randomInt))
 
 
     }
